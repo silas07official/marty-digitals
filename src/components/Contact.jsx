60c6ea75054f9434ab4033b041efd23452af6405
silas martins <silas07official@gmail.com>
2023-04-11 17:19:18 +0100

    added the required properties to input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,15 +20,14 @@ function Contact() {
       )
       .then(
         (result) => {
-          console.log(result.text)
-          setShowModal(true)
+          console.log(result.text);
+          setShowModal(true);
           e.target.reset();
         },
         (error) => console.log(error.text)
-        
       );
   };
- 
+
   return (
     <section
       id="contact"
@@ -43,59 +42,119 @@ function Contact() {
       </p>
       <div className="md:flex justify-center items-center gap-7 mt-10">
         <div className="max-w-md bg-white rounded-lg overflow-hidden md:max-w-xl">
-        <div className="md:flex">
-          <div className="md:w-1/2 bg-indigo-50 p-4">
-            <form ref={form} onSubmit={sendEmail}>
-              <div className="mb-4">
-                <label htmlFor="name" className="block text-bigText font-semibold mb-2">Name</label>
-                <input type="text" id="name" name="name" className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-bigText font-semibold mb-2">Email</label>
-                <input type="email" id="email" name="email" className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-              </div>
-              <div className="mb-4">
-                <label htmlFor="message" className="block text-bigText font-semibold mb-2">Message</label>
-                <textarea id="message" name="message" className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
-              </div>
-              <div className="text-center">
-                 <Button
-                type="submit"
-                className="mx-4 bg-indigo-500  text-white hover:text-indigo-500 hover:bg-sectionBg hover:border hover:border-indigo-500 duration-400 ease-in my-4"
-              >
-                <span>Send Message</span>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </Button>
-              </div>
-            </form>
-          </div>
-          <div className="md:w-1/2 bg-indigo-100 p-4">
-            <h2 className="text-md text-bigText md:text-lg font-semibold mb-4">Contact Information</h2>
-            <ul className=" pl-4 list-none">
-              <li className="mb-2"><ion-icon name="locate-outline" className="inline-block mr-2"></ion-icon>Lagos, Nigeria</li>
-              <li className="mb-2"><ion-icon name="call-outline" className="inline-block mr-2" ></ion-icon>+234 9066-777-091</li>
-              <li><ion-icon name="mail-outline" className="inline-block mr-2"></ion-icon>martydigitals@gmail.com</li>
-              <li><ion-icon name="logo-facebook" className="inline-block mr-2"></ion-icon>Marty Digitals</li>
-              <li><ion-icon name="logo-twitter" className="inline-block mr-2"></ion-icon>Marty Digitals</li>
-            </ul>
+          <div className="md:flex">
+            <div className="md:w-1/2 bg-indigo-50 p-4">
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="mb-4">
+                  <label
+                    htmlFor="name"
+                    className="block text-bigText font-semibold mb-2"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    required
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-bigText font-semibold mb-2"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="message"
+                    className="block text-bigText font-semibold mb-2"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    required
+                    className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  ></textarea>
+                </div>
+                <div className="text-center">
+                  <Button
+                    type="submit"
+                    className="mx-3 bg-indigo-500  text-white hover:text-indigo-500 hover:bg-sectionBg hover:border hover:border-indigo-500 duration-400 ease-in my-4"
+                  >
+                    <span>Send Message</span>
+                    <ion-icon name="paper-plane-outline"></ion-icon>
+                  </Button>
+                </div>
+              </form>
+            </div>
+            <div className="md:w-1/2 bg-indigo-100 p-4">
+              <h2 className="text-md text-bigText md:text-lg font-semibold mb-4">
+                Contact Information
+              </h2>
+              <ul className=" pl-2 list-none">
+                <li className="flex items-center gap-1  my-4 text-sm">
+                  <ion-icon
+                    name="locate-outline"
+                    className="inline-block mr-2"
+                  ></ion-icon>
+                  Lagos, Nigeria
+                </li>
+                <li className="flex items-center gap-1  my-4 text-sm">
+                  <ion-icon
+                    name="call-outline"
+                    className="inline-block mr-2"
+                  ></ion-icon>
+                  +234 9066-777-091
+                </li>
+                <li className="flex items-center gap-1 text-sm my-4">
+                  <ion-icon
+                    name="mail-outline"
+                    className="inline-block"
+                  ></ion-icon>
+                  martydigitals@gmail.com
+                </li>
+                <li className="flex items-center my-4  gap-1 text-sm">
+                  <ion-icon
+                    name="logo-facebook"
+                    className="inline-block mr-2"
+                  ></ion-icon>
+                  Marty Digitals
+                </li>
+                <li className="flex items-center my-4 gap-1 text-sm">
+                  <ion-icon
+                    name="logo-twitter"
+                    className="inline-block mr-2"
+                  ></ion-icon>
+                  Marty Digitals
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-            </div>
-            <div className="mt-5 hidden md:block md:mt-0">
-              <img src="https://www.sendnotice.in/assets/images/contactus.svg" alt="contactus_image" className="w-[100%]" />
-            </div>
+        <div className="mt-5 hidden md:block md:mt-0">
+          <img
+            src="https://www.sendnotice.in/assets/images/contactus.svg"
+            alt="contactus_image"
+            className="w-[100%]"
+          />
+        </div>
       </div>
-      {showModal?<Modal message="Message sent successfully" />:null}
+      {showModal ? <Modal message="Message sent successfully" /> : null}
     </section>
   );
 }
 
 export default Contact;
-
-
-
-
-
-
-
-
